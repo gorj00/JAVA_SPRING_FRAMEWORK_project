1976db3c47ba80ba73b2d21ced6a17c29889d9a7
Jan Gorol <os.jangorol@gmail.com>
2023-01-12 10:07:56 +0100

    annotations hooks

--- annotations_spring_configuration/TennisCoach.java
+++ annotations_spring_configuration/TennisCoach.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-@Scope("prototype")
+@Scope("singleton") //default, keep it only for demonstration
 public class TennisCoach implements Coach {
 
     // FIELD INJECTION with reflection
@@ -40,4 +43,15 @@ public class TennisCoach implements Coach {
         this.fortuneService = fortuneService;
     }
     */
+
+    @PostConstruct // os of Java 9, needs to be imported from external package
+    public void doInitOperations() {
+        System.out.println("Tennis coach: init hook");
+    }
+
+    @PreDestroy // os of Java 9, needs to be imported from external package
+    public void doDestroyOperations() {
+        System.out.println("Tennis coach: destroy hook");
+    }
+
 }

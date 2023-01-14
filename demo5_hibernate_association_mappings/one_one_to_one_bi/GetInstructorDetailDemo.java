5519803f0cd1f1f348a9050eac896cfc4195aaf0
Jan Gorol <os.jangorol@gmail.com>
2023-01-14 07:13:06 +0100

    one-to-one uni and bi

---
+++ demo5_hibernate_association_mappings/one_one_to_one_bi/GetInstructorDetailDemo.java
@@ -0,0 +1,39 @@
+package demo5_hibernate_association_mappings.one_one_to_one_bi;
+
+import demo5_hibernate_association_mappings.one_one_to_one_bi.entity.Instructor;
+import demo5_hibernate_association_mappings.one_one_to_one_bi.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetInstructorDetailDemo {
+    public static void main(String[] args) {
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("demo5_hibernate_association_mappings/one_one_to_one_uni/hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // start a transaction
+            session.beginTransaction();
+
+            int id = 1;
+
+            InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, id);
+            System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+            System.out.println("the associated instructor: " + tempInstructorDetail.getInstructor() );
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } finally {
+            factory.close();
+        }
+    }
+}
---
+++ demo5_hibernate_association_mappings/one_one_to_one_uni/CreateDemo.java
@@ -0,0 +1,49 @@
+package demo5_hibernate_association_mappings.one_one_to_one_uni;
+
+import demo4_hibernate_crud.entity.Student;
+import demo5_hibernate_association_mappings.one_one_to_one_uni.entity.Instructor;
+import demo5_hibernate_association_mappings.one_one_to_one_uni.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateDemo {
+    public static void main(String[] args) {
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("demo5_hibernate_association_mappings/one_one_to_one_uni/hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // start a transaction
+            session.beginTransaction();
+
+            // create objects
+            Instructor tempInstructor = new Instructor("Chad", "Darby", "ex@ample.com");
+            InstructorDetail tempInstructorDetail = new InstructorDetail(
+                    "http://youtube.com",
+                    "dog training"
+            );
+
+            // associate the objects
+            tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+            // save the instructor
+            // also saves the details object due to cascadeType.ALL
+            System.out.println("Saving instrucotr: " + tempInstructor);
+            session.save(tempInstructor);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } finally {
+            factory.close();
+        }
+    }
+}

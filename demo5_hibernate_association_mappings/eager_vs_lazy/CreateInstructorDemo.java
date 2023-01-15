1b70580f4309e0fb10cfecd401229eaacf914cd4
Jan Gorol <os.jangorol@gmail.com>
2023-01-15 21:03:37 +0100

    eager vs lazy

---
+++ demo5_hibernate_association_mappings/eager_vs_lazy/CreateInstructorDemo.java
@@ -0,0 +1,51 @@
+package demo5_hibernate_association_mappings.eager_vs_lazy;
+
+import demo5_hibernate_association_mappings.eager_vs_lazy.entity.InstructorDetail;
+import demo5_hibernate_association_mappings.eager_vs_lazy.entity.Course;
+import demo5_hibernate_association_mappings.eager_vs_lazy.entity.Instructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateInstructorDemo {
+    public static void main(String[] args) {
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("demo5_hibernate_association_mappings/one_to_many_bi/hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // start a transaction
+            session.beginTransaction();
+
+            // create objects
+            Instructor tempInstructor = new Instructor("Susan", "Public", "ex@ampleSusan.com");
+            InstructorDetail tempInstructorDetail = new InstructorDetail(
+                    "http://youtube.com",
+                    "gaming"
+            );
+
+            // associate the objects
+            tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+            // save the instructor
+            // also saves the details object due to cascadeType.ALL
+            System.out.println("Saving instrucotr: " + tempInstructor);
+            session.save(tempInstructor);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}

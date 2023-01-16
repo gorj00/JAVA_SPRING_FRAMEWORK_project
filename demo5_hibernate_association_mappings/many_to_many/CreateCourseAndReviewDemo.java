88c4f6de1e141f14c2df4a404f0e278a2507bf34
Jan Gorol <os.jangorol@gmail.com>
2023-01-16 20:47:51 +0100

    mamy to many

---
+++ demo5_hibernate_association_mappings/many_to_many/CreateCourseAndReviewDemo.java
@@ -0,0 +1,50 @@
+package demo5_hibernate_association_mappings.one_to_many_uni;
+
+import demo5_hibernate_association_mappings.one_to_many_uni.entity.Course;
+import demo5_hibernate_association_mappings.one_to_many_uni.entity.Instructor;
+import demo5_hibernate_association_mappings.one_to_many_uni.entity.InstructorDetail;
+import demo5_hibernate_association_mappings.one_to_many_uni.entity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateCourseAndReviewDemo {
+    public static void main(String[] args) {
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("demo5_hibernate_association_mappings/one_to_many_uni/hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            // start a transaction
+            session.beginTransaction();
+
+            // create a course
+            Course tempCourse = new Course("Pacman - How to score milion points");
+
+            // add some reviews
+            tempCourse.addReview(new Review("Rev1"));
+            tempCourse.addReview(new  Review("Rev2"));
+            tempCourse.addReview(new Review("Rev3"));
+
+            // save the course and reviews via cascade
+            System.out.println("Saving the course: " + tempCourse);
+            session.save(tempCourse);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } finally {
+            session.close();
+            factory.close();
+        }
+    }
+}

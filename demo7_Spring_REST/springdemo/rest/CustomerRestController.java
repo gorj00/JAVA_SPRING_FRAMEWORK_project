9ca2be2b0e60218ca12f67db1e663f9cde9b2e6b
Jan Gorol <os.jangorol@gmail.com>
2023-02-06 11:33:56 +0100

    rest exception handling

--- demo7_Spring_REST/springdemo/rest/CustomerRestController.java
+++ demo7_Spring_REST/springdemo/rest/CustomerRestController.java
@@ -25,6 +25,9 @@ public class CustomerRestController {
     @GetMapping("/customers/{customerId}")
     public Customer getCustomer(@PathVariable int customerId) {
         Customer theCustomer = customerService.getCustomer(customerId);
+        if (theCustomer == null) {
+            throw new CustomerNotFoundException("Customer id not found - " + customerId);
+        }
         return theCustomer;
     }
 

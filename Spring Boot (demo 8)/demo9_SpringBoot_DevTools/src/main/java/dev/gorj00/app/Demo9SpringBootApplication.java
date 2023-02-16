66febb401d60897cc4e2e74c4c740873491a8683
Jan Gorol <os.jangorol@gmail.com>
2023-02-16 13:11:06 +0100

    dev tools

--- Spring Boot (demo 8)/demo9_SpringBoot_DevTools/src/main/java/dev/gorj00/app/Demo9SpringBootApplication.java
+++ Spring Boot (demo 8)/demo9_SpringBoot_DevTools/src/main/java/dev/gorj00/app/Demo9SpringBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo8SpringBootApplication {
+public class Demo9SpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demo8SpringBootApplication.class, args);
+        SpringApplication.run(Demo9SpringBootApplication.class, args);
     }
 
 }
--- Spring Boot (demo 8)/demo9_SpringBoot_DevTools/src/main/java/dev/gorj00/app/rest/HelloRestController.java
+++ Spring Boot (demo 8)/demo9_SpringBoot_DevTools/src/main/java/dev/gorj00/app/rest/HelloRestController.java
@@ -1,5 +1,6 @@
 package dev.gorj00.app.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +8,22 @@ import java.time.LocalDateTime;
 
 @RestController
 public class HelloRestController {
+    @Value("${team.name}")
+    private String teamName;
 
     @GetMapping("/")
     public String sayHello() {
         return "Hello Spring Boot Wolrd! Time on server is " + LocalDateTime.now();
     }
 
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 30k!";
+    }
 
+    @GetMapping("/team-workout")
+    public String getTeamWorkout() {
+        return teamName + ", run a hard 30k!";
+    }
 
 }

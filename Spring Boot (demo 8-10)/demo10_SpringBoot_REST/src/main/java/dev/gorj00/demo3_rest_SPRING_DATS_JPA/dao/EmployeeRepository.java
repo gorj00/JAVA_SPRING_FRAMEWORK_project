5e77f8057d5a21366e5d6980214301145abf8320
Jan Gorol <os.jangorol@gmail.com>
2023-02-21 12:34:54 +0100

    pagination and sort with spring boot data rest

--- Spring Boot (demo 8-10)/demo10_SpringBoot_REST/src/main/java/dev/gorj00/demo3_rest_SPRING_DATS_JPA/dao/EmployeeRepository.java
+++ Spring Boot (demo 8-10)/demo10_SpringBoot_REST/src/main/java/dev/gorj00/demo3_rest_SPRING_DATS_JPA/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package dev.gorj00.demo3_rest_SPRING_DATS_JPA.dao;
 
 import dev.gorj00.demo_rest_ORIGINAL.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members") // for custom plural name or custom name
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // Alternative to writing DAO and DAOImpl code, this handles it and provides crud
 }
